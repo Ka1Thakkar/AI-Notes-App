ba4e8c9bb027c9b53361ddc6b7d047c11b84195d
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2025-04-23 00:51:46 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: {
-        '@tailwindcss/postcss': {
-            optimize: {
-                minify: false
-            }
-        },
     },
 };
 
